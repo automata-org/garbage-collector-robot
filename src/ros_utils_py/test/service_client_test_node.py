1bf11e7e174dac04b1a567e3901a2fdad4c5f4e0
michele de giulio <mdegiulio97@gmail.com>
2022-07-07 21:32:47 +0200

    Removed unnecessary imports, and modified the class import, and solved the alias problem

--- src/ros_utils_py/test/service_client_test_node.py
+++ src/ros_utils_py/test/service_client_test_node.py
@@ -1,16 +1,14 @@
-from __future__ import print_function
-
-import ros_utils_py.ros_service as GenericServiceClient
+import ros_utils_py.ros_service as ServiceClient
 import sys
 import rospy
 from std_srvs.srv import Trigger
 
 
 def main():
-    client = GenericServiceClient("talk_service", Trigger)
+    client = ServiceClient.GenericServiceClient("talk_service", Trigger)
     res1 = client.request()
     print("Service Talker ->", res1.message)   
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":s
     main()
