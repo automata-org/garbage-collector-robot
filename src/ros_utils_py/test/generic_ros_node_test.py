12d2cbaa0429c859c3f0fcc7ca22c5624d7efaae
Giorgio <giorgiocrovetti452@gmail.com>
2022-07-07 07:36:23 +0200

    Update generic_ros_node_test.py

--- src/ros_utils_py/test/generic_ros_node_test.py
+++ src/ros_utils_py/test/generic_ros_node_test.py
@@ -1,10 +1,22 @@
+#!/usr/bin/env python
+from __future__ import print_function
 import ros_utils_py.ros_node as node
 import rospy
 from std_msgs.msg import String
-from __future__ import print_function
 from std_srvs.srv import Trigger, TriggerResponse
 
 def main():
+
+    class Talker(GenericROSNode):
+        def __init__(self, name):
+            super().__init__(name)
+        def main_task(self):
+            self.get_publisher('chatter').publish("Blip!")
+
+    class Listener(GenericROSNode):
+        def __init__(self, name):
+            super().__init__(name)
+        
     talker=Talker('talker')
     talker.add_publisher('chatter',String)
     talker.run(10)
