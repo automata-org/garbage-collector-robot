bed8aa01bbad5e5195e22c5543cd9fc965176af8
Peeno22 <lpinelli37@gmail.com>
2022-07-09 11:10:54 +0200

    Removed unused libraries

--- src/ros_utils_py/src/ros_utils_py/ros_subscriber.py
+++ src/ros_utils_py/src/ros_utils_py/ros_subscriber.py
@@ -1,11 +1,8 @@
 import rospy
-from std_msgs.msg import String #why?
 
 
 class GenericSubscriber:
     def __init__(self, topic_name, data_type, callback):
         self.subscriber = rospy.Subscriber(topic_name, data_type, callback)
-    
-    def subscribe (self): #Why? subscription is already done by Subscriber constructor
-        self.subscriber.subscribe()
+       
 
