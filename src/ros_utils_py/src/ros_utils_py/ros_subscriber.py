bcfd754a1e0ac1b7a438ae8ca4827e066677d565
Michelangelo Setaro <mksetaro@gmail.com>
2022-07-09 09:16:58 +0200

    Added comments

--- src/ros_utils_py/src/ros_utils_py/ros_subscriber.py
+++ src/ros_utils_py/src/ros_utils_py/ros_subscriber.py
@@ -1,11 +1,11 @@
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String #why?
 
 
 class GenericSubscriber:
     def __init__(self, topic_name, data_type, callback):
         self.subscriber = rospy.Subscriber(topic_name, data_type, callback)
     
-    def subscribe (self):
+    def subscribe (self): #Why? subscription is already done by Subscriber constructor
         self.subscriber.subscribe()
 
