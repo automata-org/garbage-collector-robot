9d9a49b4cf7dc981be636324c0b9acfa4965508d
Michelangelo Setaro <mksetaro@gmail.com>
2022-07-09 12:01:34 +0200

    Post merge integration

--- src/ros_utils_py/src/ros_utils_py/ros_node.py
+++ src/ros_utils_py/src/ros_utils_py/ros_node.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import rospy
+import ros_utils_py.ros_publisher as publisher
+import ros_utils_py.ros_subscriber as subscriber
+import ros_utils_py.ros_service as service
 
 class GenericROSNode:
     def __init__(self, name):
@@ -19,13 +22,13 @@ class GenericROSNode:
         raise Exception("Not Implemented")
       
     def add_publisher(self,topic_name, data_type):
-        self.publishers[topic_name]=rospy.Publisher(topic_name, data_type, queue_size=1)
+        self.publishers[topic_name]=publisher.GenericPublisher(topic_name, data_type)
     def add_subscriber(self, topic_name, data_type,callback):
-        self.subscribers[topic_name]=rospy.Subscriber(topic_name, data_type, callback)
+        self.subscribers[topic_name]=subscriber.GenericSubscriber(topic_name, data_type, callback)
     def add_service_client(self, service_name, data_type):
-        self.clients[service_name]=rospy.ServiceProxy(service_name, data_type)
+        self.clients[service_name]=service.GenericServiceClient(service_name, data_type)
     def add_service_server(self, service_name, data_type, callback):
-        self.services[service_name]=rospy.Service(service_name, data_type, callback)
+        self.services[service_name]=service.GenericServiceServer(service_name, data_type, callback)
 
     def get_publisher(self,topic_name):
         try:
@@ -48,7 +51,7 @@ class GenericROSNode:
         except:
             rospy.logerr("cannot find service for service name: ", service_name)
             raise ValueError("service does not exist")
-    def get_client(self, service_name):
+    def get_service_client(self, service_name):
         try:
             client=self.clients[service_name]
             return client
