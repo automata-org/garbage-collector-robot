fb789328c49e999421248cadd66b92b4ecd6c817
Michelangelo Setaro <mksetaro@gmail.com>
2022-07-09 09:13:53 +0200

    Added comments

--- src/ros_utils_py/test/generic_ros_node_test.py
+++ src/ros_utils_py/test/generic_ros_node_test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-from __future__ import print_function
+from __future__ import print_function #why?
 import rospy
 import ros_utils_py.ros_node as node
 from std_msgs.msg import String
-from std_srvs.srv import Trigger, TriggerResponse
+from std_srvs.srv import Trigger, TriggerResponse #why?
 
 def main():
 
@@ -13,7 +13,7 @@ def main():
         def main_task(self):
             self.get_publisher('chatter').publish("Blip!")
 
-    class Listener(node.GenericROSNode):
+    class Listener(node.GenericROSNode): #not necessary and unused
         def __init__(self, name):
             super().__init__(name)
         
